a5e203a0f726cc26522c1628ee15f51023752465
mrflores56 <gokuloquendo03@gmail.com>
2025-10-27 11:24:48 -0300

    Update Main
    - Se agregó un menú secundario.
    - Bug fixes.

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,14 @@ import Model.Pokemones.Caterpie;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 
+// TP GRUPAL POKEDEX (Flores, Jimenez, Pascuan, Silvestre).
 
 public class Main {
     public static void main(String[] args) {
 
         // Variables
         Scanner sc = new Scanner(System.in);
-        int opcion;
+        int opcion, opcion1;
 
         Pokemon caterpie = new Caterpie("Marcelo");
         Pokemon caterpie2 = new Caterpie("Emanuel");
@@ -21,8 +22,9 @@ public class Main {
         Entrenador entrenador = new Entrenador("Joel","Gardel");
         GestorEquipo gestorEquipo = new GestorEquipo();
         gestorEquipo.crearEquipo(entrenador);
-        LinkedHashSet<Pokemon> mochilaDelEntrenador = gestorEquipo.crearMochilaEntrenador();
+        // LinkedHashSet<Pokemon> mochilaDelEntrenador = gestorEquipo.crearMochilaEntrenador();
 
+        saltarLinea(1);
         do {
             opcion = menu(sc);
             saltarLinea(1);
@@ -33,7 +35,39 @@ public class Main {
                 } // Salir
                 case 1: {
                     break;
-                }
+                } // Ver Pokédex
+                case 2: {
+                    opcion1 = menuEntrenador(sc);
+                    do {
+                        switch(opcion1) {
+                            case 0: {
+                                break;
+                            } // Salir
+                            case 1: {
+                                System.out.println("<Creando entrenador nuevo...>");
+                                // Código.
+                                break;
+                            } // Crear entrenador
+                            case 2: {
+                                System.out.println("<Mostrando todos los entrenadores disponibles...>");
+                                // Código.
+                                break;
+                            } // Elegir entrenador
+                            default: {
+                                System.out.println("! ERROR !");
+                                break;
+                            } // Error
+                        }
+
+                        if(opcion1 != 0) {
+                            limpiarPantalla(sc);
+                        } // Limpiar pantalla
+                    } while(opcion1 != 0);
+
+                } // Crear/Elegir entrenador
+                case 3: {
+                    break;
+                } // Batalla
                 default: {
                     System.out.println("! ERROR !");
                     break;
@@ -48,16 +82,26 @@ public class Main {
 
     // Métodos
     public static int menu(Scanner sc) {
-        saltarLinea(1);
-        System.out.println("[ POKEDEX ]");
+        System.out.println("[ SIMULADOR POKÉMON ]");
         separador(30);
-        System.out.println("[0.] SALIR");
-        System.out.println("[1.] (!) ");
-        System.out.println("[2.] (!)");
-        System.out.println("[3.] (!)");
-        System.out.println("[4.] (!)");
+        System.out.println("[0.] SALIR.");
+        System.out.println("[1.] (!) Ver Pokédex.");
+        System.out.println("[2.] (!) Crear/Elegir entrenador.");
+        System.out.println("[3.] (!) Batalla.");
         separador(30);
         System.out.print("- Ingrese una opción: ");
+        int opcion = sc.nextInt();
+        sc.nextLine();
+        return opcion;
+    }
+    public static int menuEntrenador(Scanner sc) {
+        System.out.println("[ MENU ENTRENADOR ]");
+        separador(30);
+        System.out.println("[0.] SALIR.");
+        System.out.println("[1.] (!) CREAR.");
+        System.out.println("[2.] (!) ELEGIR.");
+        separador(30);
+        System.out.print("- Ingrese una opcion: ");
         int opcion = sc.nextInt();
         sc.nextLine();
         return opcion;
